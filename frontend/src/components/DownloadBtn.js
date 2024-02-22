1274bdd982c9eda3f612997844c309c0337b73a7
Chloe Han <jh7316@nyu.edu>
2024-02-23 04:00:58 +0900

    feat : edit DownloadBtn component

--- frontend/src/components/DownloadBtn.js
+++ frontend/src/components/DownloadBtn.js
@@ -1,10 +1,14 @@
-export default function DownloadBtn ({imageUrl}) {
+export default function DownloadBtn ({imageUrl, isFetching}) {
     function handleDownload () {
-        const link = document.createElement('a');
-        link.href = imageUrl;
-        link.download = 'output.jpg';
-        link.click();
+        if(imageUrl){
+            const link = document.createElement('a');
+            link.href = imageUrl;
+            link.download = 'output.jpg';
+            link.click();
+        }else{
+            alert('There is no file to download!');
+        }
     }
 
-    return <button className="absolute bottom-8 right-8" onClick={handleDownload}>Download</button>
+    return <button className="absolute bottom-8 right-8 disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleDownload} disabled={isFetching}>Download</button>
 }
