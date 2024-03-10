56ed30f5d99be23a30d7ccb166ba0b496d29e12d
Chloe Han <jh7316@nyu.edu>
2024-03-10 23:32:00 +0900

    feat : modify styling

--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -4,9 +4,9 @@ const activeStyle = "flex flex-col mx-4 text-gray-600 text-md h-full"
 const inactiveStyle = "flex flex-col pl-3 text-black font-bold text-md"
 
 export default function Panel(){
-    return <div className="flex flex-row bg-gray-100 h-[130px] w-full ">
+    return <div className="flex flex-row bg-gray-100 h-[105px] w-full">
     <div className="flex flex-col pl-3">
-        <h1 className="text-gray-800ß text-4xl my-auto ml-5 font-bold mb-0" >목욕의 신</h1>
+        <h1 className="text-gray-800 text-2xl my-auto ml-5 font-bold mb-0" >목욕의 신</h1>
         <div className="flex flex-row mt-auto">
             <NavLink to="/assets" className={({ isActive }) => isActive ? inactiveStyle : activeStyle }>
                 {({ isActive }) => (
@@ -23,9 +23,9 @@ export default function Panel(){
         </div>
     </div>
 
-    <NavLink to="/train" className="ml-auto my-auto mr-12 rounded-full text-white h-1/2 px-8
+    <NavLink to="/train" className="ml-auto my-auto mr-12 rounded-full text-white h-[45px] px-8
     bg-gradient-to-b from-[#E9522E] via-pink-600 to-[#D58ABD] font-bold">
-        <p className="text-center my-5">
+        <p className="text-center mt-3">
          Initialize Style Reference
         </p>
     </NavLink>
