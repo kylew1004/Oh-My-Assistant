3cb4b3cc3cc5a703893a1c665aa73f5c727d87dd
Chloe Han <jh7316@nyu.edu>
2024-03-11 03:31:40 +0900

    feat : modify styling

--- frontend/src/components/CreateNewPanel.js
+++ frontend/src/components/CreateNewPanel.js
@@ -5,9 +5,9 @@ export default function CreateNewPanel({type, detail, link, img}){
     const splitType = type.split("&");
 
     
-    return <NavLink to={link} className="flex flex-col h-auto w-full m-10 bg-yellow-500 rounded-3xl overflow-hidden">
-        <h1 className="font-bold text-4xl mt-10 ml-9">{splitType[0]}<br />{splitType[1]}</h1>
-        <p className="text-xl mx-10 my-3 mb-8 h-10  text-gray-600"> {detail} </p>
+    return <NavLink to={link} className="flex flex-col h-auto w-full m-10 my-16 bg-yellow-500 rounded-3xl overflow-hidden">
+        <h2 className="font-bold text-3xl mt-10 ml-9">{splitType[0]}<br />{splitType[1]}</h2>
+        <p className="text-lg mx-9 my-3 mb-8 h-10  text-gray-600"> {detail} </p>
         <img src={img} className=" h-72 w-full" />
         <img src={arrowImg} className="h-auto w-1/5 ml-auto mr-8 mt-4" />
     </NavLink>
