7959902a568ec648ea3d6c3872629f3738854bd1
Chloe Han <jh7316@nyu.edu>
2024-03-11 03:30:35 +0900

    feat : modify panels for pose transfer

--- frontend/src/components/InputPanel2.js
+++ frontend/src/components/InputPanel2.js
@@ -1,35 +1,46 @@
 import arrowImg from '../assets/arrow.png';
 
-export default function InputPanel({imageUrl, isFetching, handleChange, handleSubmit}){
+export default function InputPanel({characterUrl, poseUrl, isFetching, handleCharacter, handlePose, handleSubmit}){
 
-    return  <div className="flex flex-row shadow-xl bg-white bg-opacity-90 rounded-3xl w-full h-full">
-        <div className="w-full h-full flex flex-col my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-5 py-4 mx-3">
-            <div className="flex flex-row mb-2">
-                <h3 className="text-black text-lg font-bold">Input Image</h3>
-                <label className="create-new border-gray-300 mb-2 inline-block px-3 py-1 cursor-pointer ml-auto rounded-full bg-yellow-500">
-                    <input className="hidden" type="file" onChange={handleChange} />
-                    <p className="font-bold text-sm">Upload Image</p>
-                </label>
+    return  <div className="flex flex-col shadow-xl bg-white bg-opacity-90 rounded-3xl h-[95%] m-5 p-3 gap-4">
+        <div className="flex flex-row h-full">
+            <div className="w-full h-full flex-grow flex-col pb-16 my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-5 py-4 mx-3">
+                <div className="flex flex-row mb-2">
+                    <h3 className="text-black text-lg font-bold">Target Character</h3>
+                    <label className="create-new border-gray-300 mb-2 inline-block px-3 py-1 cursor-pointer ml-auto rounded-full bg-yellow-500">
+                        <input className="hidden" type="file" onChange={handleCharacter} />
+                        <p className="font-bold text-sm">Upload Image</p>
+                    </label>
+                </div>
+                <span className="h-full mb-3 flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+                    {characterUrl && <img className="h-full w-full object-contain mx-auto" src={characterUrl}/>}
+                </span>
+
+
             </div>
-            <span className="h-4/6 mb-3 flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-                {imageUrl && <img className="h-full w-full object-contain mx-auto" src={imageUrl}/>}
-            </span>
-
+            <div className="w-full h-full flex-grow flex-col pb-16 my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-5 py-4 mx-3">
+                <div className="flex flex-row mb-2">
+                    <h3 className="text-black text-lg font-bold">Target Pose</h3>
+                    <label className="create-new border-gray-300 mb-2 inline-block px-3 py-1 cursor-pointer ml-auto rounded-full bg-yellow-500">
+                        <input className="hidden" type="file" onChange={handlePose} />
+                        <p className="font-bold text-sm">Upload Image</p>
+                    </label>
+                </div>
+                <span className="h-full mb-3 flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+                    {poseUrl && <img className="h-full w-full object-contain mx-auto" src={poseUrl}/>}
+                </span>
+            </div>
 
         </div>
-        <div className="w-full h-full flex flex-col my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-5 py-4 mx-3">
-            <div className="flex flex-row mb-2">
-                <h3 className="text-black text-lg font-bold">Input Image</h3>
-                <label className="create-new border-gray-300 mb-2 inline-block px-3 py-1 cursor-pointer ml-auto rounded-full bg-yellow-500">
-                    <input className="hidden" type="file" onChange={handleChange} />
-                    <p className="font-bold text-sm">Upload Image</p>
-                </label>
-            </div>
-            <span className="h-4/6 mb-3 flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-                {imageUrl && <img className="h-full w-full object-contain mx-auto" src={imageUrl}/>}
-            </span>
-        </div>
-        <img className="h-9 mt-4 ml-auto my-auto disabled:bg-gray-400 cursor-pointer disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleSubmit} disabled={isFetching} src={arrowImg} />
+        <button className="flex flex-row h-12 w-full ml-auto bg-yellow-500 font-bold rounded-full disabled:bg-gray-400 disabled:text-gray-700 disabled:cursor-not-allowed"
+            onClick={handleSubmit}>
+            <p className="flex flex-row m-auto">
+                RUN
+                <img className="h-4 mt-1.5 ml-3 transform rotate-90" onClick={handleSubmit} disabled={isFetching} src={arrowImg} />
+            </p>
+            
+        </button>
+        
 
     </div>
 
--- frontend/src/components/OutputPanel2.js
+++ frontend/src/components/OutputPanel2.js
@@ -4,9 +4,6 @@ import Error from './Error.js';
 import SaveAssetModal from './SaveAssetModal.js';
 
 export default function OutputPanel({imageUrl, isFetching, error}){
-    const [activeImage, setActiveImage] = useState();
-    const [isSelect, setIsSelect] = useState(false);
-    const [selected, setSelected] = useState([]);
     const [isModal, setIsModal] = useState(false);
 
     let styling = "h-full w-full object-contain mx-auto"
@@ -15,65 +12,39 @@ export default function OutputPanel({imageUrl, isFetching, error}){
         styling="h-1/6 object-contain mx-auto";
     }
 
-    useEffect(()=>{
-        if(imageUrl) setActiveImage(imageUrl[0]);
-    },[imageUrl]);
-
-    function handleSelect(item){
-        setSelected(prev=>{
-            let updated=[...prev];
-            if(updated.includes(item)) updated = updated.filter(item1 => item1 !== item);
-            else updated.push(item);
-            return updated; 
-        });
-    }
-
     function handleSubmit(){
-        if(!isSelect) setIsSelect(true);
-        else{
-            if(selected.length===0) alert("Please select at leat one image!");
-            else setIsModal(true);
-
-        } 
+        setIsModal(true); 
     }
 
     function handleClose(){
         setIsModal(false);
-        setIsSelect(false);
-        setSelected([]);
     }
 
-    function handleCancel(){
-        setIsSelect(false);
-        setSelected([]);
-    }
 
     return  <>
             <SaveAssetModal open={isModal} handleClose={handleClose} />
         
        
 
-        <div className="w-full h-full my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-4 py-5 mx-3">
-        {error ? <Error message={error.message} /> : 
-         <span className=" h-[75%] flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-         {isFetching ? <img className={styling} src={imageUrl}/> 
-            : (activeImage && <img className={styling} src={activeImage}/>) }
-         
-        </span>}
-        <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-         {imageUrl && !isFetching && imageUrl.map((item,index)=>{
-            return <li className="relative inline-block"key={index}>
-                <img className={`h-14 w-14 rounded-xl ${activeImage===item && " border-4 border-yellow-500"}`} src={item} onClick={ (()=>setActiveImage(item))}/>
-                {isSelect && <button className={`absolute top-0 right-0 transform translate-y--2 -translate-x--1 border-2  bg-white bg-opacity-80 rounded-full ${!selected.includes(item) ? 'px-3 border-gray-500' : 'px-2 text-green-600 border-green-600'} text-md`}
-                            onClick={()=>handleSelect(item)}>{selected.includes(item) ? 'v' : <br />}</button> }
-            </li>
-         })}
-        </ul>
-        {/* <DownloadBtn imageUrl={imageUrl} isFetching={isFetching}/> */}
-        <div className="flex flex-row">
-            {isSelect && <button className="mr-auto my-auto rounded-full bg-red-500 px-5 text-white py-1 mt-4" onClick={handleCancel}>Cancel</button>}
-            <button className="ml-auto my-auto rounded-full bg-yellow-500 px-5 py-1 mt-4 mr-1 disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleSubmit} disabled={isFetching || !imageUrl}>{!isSelect ? 'Select': 'Save'}</button>
-        </div>
+        <div className="flex flex-row shadow-xl bg-white bg-opacity-90 rounded-3xl h-[95%] m-5 p-5 gap-4">
+            {error ? <Error message={error.message} /> : 
+            <span className=" h-full w-[55%] flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+            {isFetching ? <img className={styling} src={imageUrl}/> 
+                : (imageUrl && <img className={styling} src={imageUrl[0]}/>) }
+            
+            </span>}
+
+            <div className="flex flex-col w-[45%] h-full gap-3">
+                <p className="text-black text-lg font-bold">Process Image</p>
+                <span className=" h-full w-full flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+                {isFetching ? <img className={styling} src={imageUrl}/> 
+                    : (imageUrl && <img className={styling} src={imageUrl[1]}/>) }
+                
+                </span>
+                {/* <DownloadBtn imageUrl={imageUrl} isFetching={isFetching}/> */}
+                <button className="m-auto w-full h-16 rounded-full bg-yellow-500 px-5 py-1 mt-4 mr-1 disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleSubmit} disabled={isFetching || !imageUrl}>Save</button>
+            </div>
+            
         
     </div>
     
