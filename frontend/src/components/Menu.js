b5ce253cd3f3c671c54544d1c24f248c035dd798
Chloe Han <jh7316@nyu.edu>
2024-03-10 23:26:08 +0900

    feat : delete stats section & add modal

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -2,48 +2,59 @@ import {useState} from 'react';
 import {Form} from 'react-router-dom';
 import menuLogo from '../assets/menu-logo.png';
 import profileImg from '../assets/profile.png';
+import AddWebtoonModal from './AddWebtoonModal';
 
 export default function Menu(){
+    const [isModal, setIsModal] = useState(false);
 
-    return<div className="flex flex-col bg-gray-900 h-screen w-1/6 float-left overflow-hidden">
-        <div className="flex flex-row m-2 ml-0 pl-4 pt-5">
-            <img src={menuLogo} className=" w-10 h-auto p-2"/>
-            <h3 className="text-gray-200 p-2 text-xl" >MENU</h3>
+    function handleClick(){
+        setIsModal(true);
+    }
+
+    function handleClose(){
+        setIsModal(false);
+    }
+
+    return<div className="flex flex-col bg-gradient-to-bl from-[#0f0417] to-[#24263d] h-screen w-[220px] float-left overflow-hidden">
+        <AddWebtoonModal open={isModal} handleClose={handleClose} />
+        <div className="flex flex-row m-1 pl-4 pt-2">
+            <img src={menuLogo} className=" w-9 h-9 p-1 mt-[1px] mr-2"/>
+            <h3 className="text-gray-200 mt-2 text-md" >MENU</h3>
         </div>
 
-        <div className="flex flex-row m-2 ml-0 pl-4 bg-gray-950 py-4">
-            <img src={profileImg} className=" w-14 h-14 p-2"/>
+        <div className="flex flex-row mt-3  pl-4 bg-gray-950 py-4">
+            <img src={profileImg} className="w-9 h-9 mt-1"/>
             <div className="flex flex-col pl-3">
-                <h3 className="text-white text-lg pb-1" >김작가</h3>
-                <p className=" text-gray-600 text-md"> jakga@gmail.com </p>
+                <h3 className="text-white text-md pb-1" >김작가</h3>
+                <p className=" text-gray-600 text-sm"> jakga@gmail.com </p>
             </div>
         </div>
 
-        <div className="flex flex-row m-2 ml-0 pl-4 ">
+        {/* <div className="flex flex-row m-2 ml-0 pl-4 ">
             <div className="flex flex-col pl-3">
-                <h3 className="text-white text-lg pb-1" >4</h3>
-                <p className=" text-gray-600 text-md"> WEBTOONS </p>
+                <h3 className="text-white text-md pb-1" >4</h3>
+                <p className=" text-gray-600 text-sm"> WEBTOONS </p>
             </div>
             <div className="flex flex-col pl-3">
                 <h3 className="text-white text-lg pb-1" >34</h3>
                 <p className=" text-gray-600 text-md"> ASSETS </p>
             </div>
-        </div>
+        </div> */}
 
 
-        <div className="flex flex-col m-2 ml-0 pl-4 py-4">
+        <div className="flex flex-col pl-4 py-4">
             <p className=" text-gray-600 text-md my-3"> WEBTOONS </p>
-            <div className="flex flex-col pl-3 py-1">
-                <button className="text-white text-lg pb-1  w-full text-left hover:bg-gray-950" >웹툰 1</button>
+            <div className="flex flex-col pl-3 p-1">
+                <button className="text-white text-md pb-1  w-full text-left hover:bg-gray-950" >웹툰 1</button>
             </div>
-            <div className="flex flex-col pl-3 py-1">
-                <button className="text-white text-lg pb-1  w-full text-left hover:bg-gray-950" >웹툰 2</button>
+            <div className="flex flex-col pl-3 pb-1">
+                <button className="text-white text-md pb-1  w-full text-left hover:bg-gray-950" >웹툰 2</button>
             </div>
-            <div className="flex flex-col pl-3 py-1">
-            <button className="text-white text-lg pb-1  w-full text-left hover:bg-gray-950" >웹툰 3</button>
+            <div className="flex flex-col pl-3 pb-1">
+            <button className="text-white text-md pb-1  w-full text-left hover:bg-gray-950" >웹툰 3</button>
             </div>
 
-            <button className="text-yellow-500 text-lg pb-1 bg-transparent text-left pl-2 mt-3">+ Add New Webtoon</button>
+            <button className="text-yellow-500 text-md pb-1 bg-transparent text-left mt-3" onClick={handleClick}>+ Add New Webtoon</button>
         </div>
 
         <Form action="/logout" method="post" className="flex flex-col h-full">
