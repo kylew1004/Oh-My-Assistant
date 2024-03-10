9ffd9ce04318f97c31379d4fd59aef4220f968fa
Chloe Han <jh7316@nyu.edu>
2024-03-10 23:30:04 +0900

    feat : modify styling

--- frontend/src/components/CreateNewPanel.js
+++ frontend/src/components/CreateNewPanel.js
@@ -5,10 +5,10 @@ export default function CreateNewPanel({type, detail, link, img}){
     const splitType = type.split("&");
 
     
-    return <NavLink to={link} className="flex flex-col h-auto w-4/6 m-20 bg-yellow-500 rounded-3xl overflow-hidden">
-        <h1 className="font-bold text-5xl mx-auto mt-16">{splitType[0]}<br />{splitType[1]}</h1>
-        <p className="text-xl mx-20 my-7 h-10  text-gray-600"> {detail} </p>
-        <img src={img} className="h-auto w-full" />
-        <img src={arrowImg} className="h-auto w-1/5 ml-auto mr-8 mt-7" />
+    return <NavLink to={link} className="flex flex-col h-auto w-full m-10 bg-yellow-500 rounded-3xl overflow-hidden">
+        <h1 className="font-bold text-4xl mt-10 ml-9">{splitType[0]}<br />{splitType[1]}</h1>
+        <p className="text-xl mx-10 my-3 mb-8 h-10  text-gray-600"> {detail} </p>
+        <img src={img} className=" h-72 w-full" />
+        <img src={arrowImg} className="h-auto w-1/5 ml-auto mr-8 mt-4" />
     </NavLink>
 }
--- frontend/src/page/StyleTransfer.js
+++ frontend/src/page/StyleTransfer.js
@@ -4,6 +4,7 @@ import OutputPanel from '../components/OutputPanel.js';
 import InputPanel from '../components/InputPanel.js';
 
 import { fetchOutput, postInput } from '../util/http.js';
+import { postStyleTransfer } from "../util/http.js";
  
 function StyleTransfer() {
     const [file, setFile] = useState();
@@ -28,10 +29,10 @@ function StyleTransfer() {
         setErrorFetching(null);
         setIsFetching(true);
 
-        postInput(file)
-          .then(() =>{
-            return fetchOutput();
-          })
+        postStyleTransfer(file)
+          // .then(() =>{
+          //   return fetchOutput();
+          // })
           .then(imgUrl =>{
             console.log(imgUrl);
             setOutputUrl(imgUrl);
@@ -55,11 +56,13 @@ function StyleTransfer() {
  
     return (
         <>
-        {/* <header className="flex font-sans flex-row items-center justify-center p-16">
-          <h2>Style Transfer demo</h2>
-        </header> */}
+        <header className="flex font-sans flex-row items-center justify-center pt-5 pb-3">
+          <h2 className="text-gray-600">SCENE STYLE TRANSFER</h2>
+          {/* <button>Back</button> */}
+        </header>
+        <hr className="bg-gray-700 h-[0.4px] w-11/12 mx-auto" />
 
-        <div className="flex font-sans flex-row items-center justify-center h-screen my-auto py-10">
+        <div className="flex font-sans flex-row items-center justify-center h-screen my-auto pt-6 pb-3">
           <InputPanel imageUrl={fileUrl} isFetching={isFetching} handleChange={handleChange} handleSubmit={handleSubmit}/>
           <OutputPanel imageUrl={outputUrl} isFetching={isFetching} error={errorFetching} />    
         </div>
