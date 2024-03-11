eda5bdb3f7b056d849a6e25f2984ccca51a46ae0
Chloe Han <jh7316@nyu.edu>
2024-03-12 04:30:26 +0900

    feat : create webtoon name params in path

--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -1,20 +1,22 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 
 const activeStyle = "flex flex-col mx-4 text-gray-600 text-md h-full"
 const inactiveStyle = "flex flex-col pl-3 text-black font-bold text-md"
 
 export default function Panel(){
+    const {webtoonName} = useParams();
+
     return <div className="flex flex-row bg-gray-100 h-[105px] w-full">
     <div className="flex flex-col pl-3">
-        <h1 className="text-gray-800 text-2xl my-auto ml-5 font-bold mb-0" >목욕의 신</h1>
+        <h1 className="text-gray-800 text-2xl my-auto ml-5 font-bold mb-0" >{webtoonName}</h1>
         <div className="flex flex-row mt-auto">
-            <NavLink to="/assets" className={({ isActive }) => isActive ? inactiveStyle : activeStyle }>
+            <NavLink to={`/${webtoonName}/assets`} className={({ isActive }) => isActive ? inactiveStyle : activeStyle }>
                 {({ isActive }) => (
                     isActive ? <div><p>Assets</p><hr className="bg-yellow-500 h-1.5 bottom-0 mt-1 justify-end" /></div> 
                     : <p>Assets</p>
                 )}
             </NavLink>
-            <NavLink to="/createNew" className={({ isActive }) => isActive ? inactiveStyle : activeStyle }>
+            <NavLink to={`/${webtoonName}/createNew`} className={({ isActive }) => isActive ? inactiveStyle : activeStyle }>
                 {({ isActive }) => (
                     isActive ? <div><p>Create new</p><hr className="bg-yellow-500 h-1.5 bottom-0 mt-1 justify-end" /></div> 
                     : <p>Create new</p>
@@ -23,7 +25,7 @@ export default function Panel(){
         </div>
     </div>
 
-    <NavLink to="/train" className="ml-auto my-auto mr-12 rounded-full text-white h-[45px] px-8
+    <NavLink to={`/${webtoonName}/train`} className="ml-auto my-auto mr-12 rounded-full text-white h-[45px] px-8
     bg-gradient-to-b from-[#E9522E] via-pink-600 to-[#D58ABD] font-bold">
         <p className="text-center mt-3">
          Initialize Style Reference
--- frontend/src/page/CreateNew.js
+++ frontend/src/page/CreateNew.js
@@ -1,9 +1,12 @@
+import {useParams} from 'react-router-dom';
 import styleTransferImg from '../assets/style-transfer.png';
 import poseTransferImg from '../assets/pose-transfer.png';
 import CreateNewPanel from '../components/CreateNewPanel.js';
 
 
 export default function CreateNew(){
+    const {webtoonName} = useParams();
+
     return <div className="flex flex-row h-full w-full overflow-auto">
         {/* <NavLink to="/styleTransfer" className="flex flex-col h-11/12 w-11/12 m-20 bg-yellow-500 rounded-3xl">
             <h1 className="font-bold text-5xl mx-auto mt-16">SCENE<br/>STYLE TRANSFER</h1>
@@ -12,12 +15,12 @@ export default function CreateNew(){
             <img src={arrowImg} className="h-auto w-1/5 ml-auto mr-8 mt-7" />
         </NavLink> */}
 
-        <CreateNewPanel link="/createNew/styleTransfer" 
+        <CreateNewPanel link={`/${webtoonName}/createNew/styleTransfer`} 
             type="SCENE&STYLE TRANSFER"
             detail="학습된 웹툰의 그림체로 실사 배경 이미지를 웹툰 배경으로 변환합니다."
             img={styleTransferImg}/>
         
-        <CreateNewPanel link="/createNew/poseTransfer" 
+        <CreateNewPanel link={`/${webtoonName}/createNew/poseTransfer`} 
             type="CHARACTER&POSE TRANSFER"
             detail="특정 캐릭터를 원하는 포즈로 재생산합니다."
             img={poseTransferImg}/>
