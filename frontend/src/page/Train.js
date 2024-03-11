c0432230965b74dfe210017912c35616bb4778f7
Chloe Han <jh7316@nyu.edu>
2024-03-11 15:18:53 +0900

    feat : add handleSubmit

--- frontend/src/page/Train.js
+++ frontend/src/page/Train.js
@@ -1,8 +1,10 @@
-import {Form} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import {useState} from 'react';
+import {postModelTrain} from '../util/http.js';
 
 export default function Train(){
     const [files,setFiles] = useState([]);
+    const navigate = useNavigate();
     // const urls = files.map((item)=>{
     //     return URL.createObjectURL(item);
     // })
@@ -25,13 +27,31 @@ export default function Train(){
 
     }
 
+    async function handleSubmit(){
+        const data = new FormData();
+        data.append('webtoonName','웹툰명');
+        // data.append('images', files);
+        files.forEach((file)=>{
+            data.append('images', file);
+        });
+        
+        const result = await postModelTrain(data);
+
+        // console.log(result);
+        // return null
+
+
+        navigate('/assets');
+
+    }
+
     return <div className="flex flex-col w-auto h-full bg-gradient-to-tl from-[#5748B9] to-[#624F77] m-6 mx-8 rounded-3xl p-7 px-16 overflow-auto">
         <h1 className=" text-white font-bold text-2xl mx-auto mb-6">Add your illustrations</h1>
         <p className="text-gray-400 text-md mx-auto">해당 웹툰의 그림체, 채색 스타일이 잘 드러나는 웹툰 이미지를 업로드해주세요. 업로드 된 사진들은 모델 학습에 사용됩니다. <br /> (5장 이하로 업로드)</p>
         
-        <Form className="flex flex-col h-full">
+        <div className="flex flex-col h-full">
         <label className={`mx-auto my-4 rounded-full px-10 ${files.length>4 ? "text-gray-700 bg-gray-500" : "text-[#342C5A] bg-gradient-to-r from-[#F6C443] to-[#F3AC58] font-bold cursor-pointer"}`}>
-            <input className="hidden" type="file" multiple onChange={handleChange} disabled={files.length>4}/>
+            <input className="hidden" type="file" id="input" name="input" multiple onChange={handleChange} disabled={files.length>4}/>
             <p className="mx-1 my-2">Upload Image</p>
         </label>
 
@@ -44,17 +64,20 @@ export default function Train(){
                             <img className="h-[170px] w-[170px]" src={URL.createObjectURL(item)}/> 
                             <button className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 border-2 border-gray-500 text-gray-500 bg-white bg-opacity-50 rounded-full px-2 text-lg font-bold"
                             onClick={()=>handleImageClick(index)}>x</button> 
+                            <input type="file" id="images" name="images" filename="hey" hidden/>
                         </li>
                     })
                 }
 
         </ul>
 
-        <button className="mx-auto my-auto mb-auto rounded-full text-[#342C5A] text-xl h-auto py-3 px-12 mt-8
-    bg-gradient-to-r from-[#F19E39] to-[#E34F6B] font-bold cursor-pointer">Start Model Training</button>
-        </Form>
+        <button onClick={handleSubmit} disabled={files.length==0} className="mx-auto my-auto mb-auto rounded-full text-[#342C5A] text-xl h-auto py-3 px-12 mt-8
+    bg-gradient-to-r from-[#F19E39] to-[#E34F6B] font-bold cursor-pointer disabled:bg-gray-600">Start Model Training</button>
+        </div>
         
 
         
     </div>
 }
+
+  
