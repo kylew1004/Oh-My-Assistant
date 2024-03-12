04b605f9e0cbc0a522d4bacd05ad818b1e70fb49
Chloe Han <jh7316@nyu.edu>
2024-03-13 06:10:46 +0900

    refactor : modify indent

--- frontend/src/components/OutputPanel.js
+++ frontend/src/components/OutputPanel.js
@@ -57,29 +57,29 @@ export default function OutputPanel({images, isFetching, error, originalImg}){
         
        
         <div className="w-full h-full my-auto relative shadow-xl bg-white bg-opacity-90  border-#7264a9 rounded-3xl p-4 py-5 mx-3">
-        {error ? <Error message={error.message} /> : 
-         <span className=" h-[75%] flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-         {isFetching ? <img className={styling} src={images}/> 
-            : (activeImage && <img className={styling} src={activeImage}/>) }
-         
-        </span>}
-        <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-         {images && !isFetching && images.map((item,index)=>{
-            console.log(item);
-            return <li className="relative inline-block" key={index}>
-                <img className={`h-14 w-14 rounded-xl ${activeImage===item.url && " border-4 border-yellow-500"}`} src={item.url} onClick={ (()=>setActiveImage(item.url))}/>
-                {isSelect && <button className={`absolute top-0 right-0 transform translate-y--2 -translate-x--1 border-2  bg-white bg-opacity-80 rounded-full ${!selected.includes(item) ? 'px-3 border-gray-500' : 'px-2 text-green-600 border-green-600'} text-md`}
-                            onClick={()=>handleSelect(item)}>{selected.includes(item) ? 'v' : <br />}</button> }
-            </li>
-         })}
-        </ul>
-        {/* <DownloadBtn imageUrl={imageUrl} isFetching={isFetching}/> */}
-        <div className="flex flex-row">
-            {isSelect && <button className="mr-auto my-auto rounded-full bg-red-500 px-5 text-white py-1 mt-4" onClick={handleCancel}>Cancel</button>}
-            <button className="ml-auto my-auto rounded-full bg-yellow-500 px-5 py-1 mt-4 mr-1 disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleSubmit} disabled={isFetching || !images}>{!isSelect ? 'Select': 'Save'}</button>
-        </div>
+            {error ? <Error message={error.message} /> : 
+            <span className=" h-[75%] flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+            {isFetching ? <img className={styling} src={images}/> 
+                : (activeImage && <img className={styling} src={activeImage}/>) }
+            
+            </span>}
+            <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+            {images && !isFetching && images.map((item,index)=>{
+                console.log(item);
+                return <li className="relative inline-block" key={index}>
+                    <img className={`h-14 w-14 rounded-xl ${activeImage===item.url && " border-4 border-yellow-500"}`} src={item.url} onClick={ (()=>setActiveImage(item.url))}/>
+                    {isSelect && <button className={`absolute top-0 right-0 transform translate-y--2 -translate-x--1 border-2  bg-white bg-opacity-80 rounded-full ${!selected.includes(item) ? 'px-3 border-gray-500' : 'px-2 text-green-600 border-green-600'} text-md`}
+                                onClick={()=>handleSelect(item)}>{selected.includes(item) ? 'v' : <br />}</button> }
+                </li>
+            })}
+            </ul>
+            {/* <DownloadBtn imageUrl={imageUrl} isFetching={isFetching}/> */}
+            <div className="flex flex-row">
+                {isSelect && <button className="mr-auto my-auto rounded-full bg-red-500 px-5 text-white py-1 mt-4" onClick={handleCancel}>Cancel</button>}
+                <button className="ml-auto my-auto rounded-full bg-yellow-500 px-5 py-1 mt-4 mr-1 disabled:text-gray-700 disabled:cursor-not-allowed" onClick={handleSubmit} disabled={isFetching || !images}>{!isSelect ? 'Select': 'Save'}</button>
+            </div>
         
-    </div>
+        </div>
     
     </>
 }
