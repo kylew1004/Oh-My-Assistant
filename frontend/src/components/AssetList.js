6b40035124a5664dd0f7fa70a32a7bcf29bda785
Chloe Han <jh7316@nyu.edu>
2024-03-13 21:55:22 +0900

    feat : add logout when token not found

--- frontend/src/components/AssetList.js
+++ frontend/src/components/AssetList.js
@@ -1,10 +1,12 @@
-import {Await, defer, useSearchParams, useLoaderData} from 'react-router-dom';
+import {Await, defer, useSearchParams, useLoaderData, redirect} from 'react-router-dom';
 import {Suspense, useEffect} from 'react';
 import Asset from '../components/Asset.js';
 import { getStyleAssets, getPoseAssets } from '../util/http';
+import { getAuthToken } from '../util/auth.js';
 
 export default function AssetList(){
     const {assets} = useLoaderData();
+    console.log(assets);
 
     return <div className="p-5 flex flex-row justify-start gap-5 flex-wrap mx-auto">
          <Suspense fallback={<h3 className="text-md pb-1 my-auto" >loading...</h3>}>
@@ -17,6 +19,9 @@ export default function AssetList(){
 }
 
 export function loader({params, request}){
+    const token = getAuthToken();
+    if(!token || token=='EXPIRED') return redirect('/auth');
+
     const url = new URL(request.url);
     const searchTerm = url.searchParams.get("mode");
 
