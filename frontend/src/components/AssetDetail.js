ffe7700d5175fc8e51242368adfaa867dca5a511
Chloe Han <jh7316@nyu.edu>
2024-03-14 16:23:31 +0900

    feat : load detail from server

--- frontend/src/components/AssetDetail.js
+++ frontend/src/components/AssetDetail.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import {defer} from 'react-router-dom';
+import {useState, Suspense} from 'react';
+import {Await, defer, useLoaderData} from 'react-router-dom';
 import {getPoseAlbum, getStyleAlbum} from '../util/http.js';
 
 const urls=["https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true", 
@@ -11,50 +11,70 @@ const urls=["https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypas
 ];
 
 export default function AssetDetail(){
-    const [activeImage, setActiveImage] = useState(urls[0]);
+    const [activeImage, setActiveImage] = useState();
+    const {mode, asset} = useLoaderData();
+    console.log(asset);
 
     //에셋명, 상세설명, 생성날짜, 원본이미지
 
     return <div className="flex flex-row h-full w-full bg-[#1c1a2e]">
-    <div className="flex flex-col h-full w-full rounded-l-xl p-2 justify-between">
-        <span className=" h-[90%] flex rounded-lg p-3 bg-violet-300 bg-opacity-60 justify-center items-center object-contain mx-auto" >
-            <img className="h-full w-full object-contain mx-auto" src={activeImage}/>
-        </span>
-        <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
-        {urls.map((item,index)=>{
-            console.log(item);
-            return <li className="relative inline-block" key={index}>
-                <img className={`h-14 w-14 rounded-xl ${activeImage===item && " border-4 border-yellow-500"}`} src={item} onClick={ (()=>setActiveImage(item))}/>
-            </li>
-        })}
-        </ul>
-    </div>
 
-    <div className="h-full w-full rounded-r-xl flex flex-col p-5 mx-auto">
-    <h2 className="text-gray-200">DETAILS</h2>
-    <div className="h-full w-full rounded-r-xl flex flex-col mx-auto gap-3 overflow-auto">
-        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
-            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Asset Name</h3>
-            <p className="flex-1 text-center py-3 text-gray-300">에셋명</p>
-        </div>
-        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
-            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Description</h3>
-            <p className="flex-1 text-center py-3 text-gray-300">설명설명설명~</p>
-        </div>
-        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
-            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Created At</h3>
-            <p className="flex-1 text-center py-3 text-gray-300">2024.03.14</p>
-        </div>
-        <div className="flex flex-col w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center gap-2">
-            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Input Image</h3>
-            <img className="rounded-md" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
-        </div>
+        <Suspense fallback={<h3 className="text-md pb-1 my-auto" >loading...</h3>}>
+               <Await resolve={mode} >
+                    {(loadedMode) =>  <Suspense fallback={<h3 className="text-md pb-1 my-auto" >loading...</h3>}>
+                        <Await resolve={asset} >
+                                {(loadedAsset) =>  loadedAsset && <div className="flex flex-row">
 
-    </div>
-
-    
-    </div>
-    </div>;
+                                    <div className="flex flex-col h-full w-full rounded-l-xl p-2 justify-between">
+                                   <span className=" h-[90%] w-full flex rounded-lg p-3 bg-violet-300 bg-opacity-60 justify-center items-center object-contain mx-auto" >
+                                       <img className="h-full w-full object-contain mx-auto" src={activeImage}/>
+                                   </span>
+                                   <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+                                        <li className="relative inline-block" >
+                                           <img className={`h-14 w-14 rounded-xl ${activeImage===loadedAsset.characterImgUrl && " border-4 border-yellow-500"}`} src={loadedAsset.characterImgUrl} onClick={ (()=>setActiveImage(loadedAsset.characterImgUrl))}/>
+                                       </li>
+                                       <li className="relative inline-block" >
+                                           <img className={`h-14 w-14 rounded-xl ${activeImage===loadedAsset.poseImgUrl && " border-4 border-yellow-500"}`} src={loadedAsset.poseImgUrl} onClick={ (()=>setActiveImage(loadedAsset.poseImgUrl))}/>
+                                       </li>
+                                   </ul>
+                               </div>
+                           
+                               <div className="h-full w-full rounded-r-xl flex flex-col p-5 mx-auto">
+                               <h2 className="text-gray-200">DETAILS</h2>
+                               <div className="h-full w-full rounded-r-xl flex flex-col mx-auto gap-3 overflow-auto">
+                                   <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+                                       <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Asset Name</h3>
+                                       <p className="flex-1 text-center py-3 text-gray-300">{loadedAsset.assetName}</p>
+                                   </div>
+                                   <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+                                       <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Description</h3>
+                                       <p className="flex-1 text-center py-3 text-gray-300">{loadedAsset.description}</p>
+                                   </div>
+                                   <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+                                       <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Created At</h3>
+                                       <p className="flex-1 text-center py-3 text-gray-300">{loadedAsset.createdAt}</p>
+                                   </div>
+                                   <div className="flex flex-col w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center gap-2">
+                                       <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Input Character Image</h3>
+                                       <img className="rounded-md" src={loadedAsset.originalCharacterImgUrl} />
+                                   </div>
+                                   <div className="flex flex-col w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center gap-2">
+                                       <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Input Pose Image</h3>
+                                       <img className="rounded-md" src={loadedAsset.originalPoseImgUrl} />
+                                   </div>
+                           
+                               </div>
+                           
+                               
+                               </div>
+                                </div>
+                                }
+                            </Await>
+                    </Suspense>}
+                </Await>
+        </Suspense>
+        
+        </div>;
 }
 
 export function loader({params, request}){
@@ -63,15 +83,16 @@ export function loader({params, request}){
 
     const data={
         webtoonName: params.webtoonName,
+        assetName: params.assetName,
     }
 
     if(searchTerm=='Scenes') return defer({
         mode:searchTerm,
-        assets: getStyleAlbum(data),
+        asset: getStyleAlbum(data),
       });
 
     return defer({
         mode:searchTerm,
-        assets: getPoseAlbum(data),
+        asset: getPoseAlbum(data),
       })
 }
