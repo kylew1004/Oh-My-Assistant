a632c88462aff16c766513cb32afb6c4731958b3
Chloe Han <jh7316@nyu.edu>
2024-03-14 11:27:35 +0900

    feat : add styling

--- frontend/src/components/AssetDetail.js
+++ frontend/src/components/AssetDetail.js
@@ -13,9 +13,11 @@ const urls=["https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypas
 export default function AssetDetail(){
     const [activeImage, setActiveImage] = useState(urls[0]);
 
-    return <div className="flex flex-row h-full w-full">
-    <div className="flex flex-col h-full w-full bg-white rounded-l-xl p-5 justify-between">
-        <span className=" h-[90%] flex border rounded-lg p-3 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
+    //에셋명, 상세설명, 생성날짜, 원본이미지
+
+    return <div className="flex flex-row h-full w-full bg-[#1c1a2e]">
+    <div className="flex flex-col h-full w-full rounded-l-xl p-2 justify-between">
+        <span className=" h-[90%] flex rounded-lg p-3 bg-violet-300 bg-opacity-60 justify-center items-center object-contain mx-auto" >
             <img className="h-full w-full object-contain mx-auto" src={activeImage}/>
         </span>
         <ul className="h-[14%] gap-3 mt-3 flex flex-row rounded-lg p-3 pr-5 bg-violet-300 bg-opacity-20 justify-center items-center object-contain mx-auto" >
@@ -28,8 +30,29 @@ export default function AssetDetail(){
         </ul>
     </div>
 
-    <div className="h-full w-full bg-white rounded-r-xl flex flex-col p-5">
-    DETAILS
+    <div className="h-full w-full rounded-r-xl flex flex-col p-5 mx-auto">
+    <h2 className="text-gray-200">DETAILS</h2>
+    <div className="h-full w-full rounded-r-xl flex flex-col mx-auto gap-3 overflow-auto">
+        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Asset Name</h3>
+            <p className="flex-1 text-center py-3 text-gray-300">에셋명</p>
+        </div>
+        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Description</h3>
+            <p className="flex-1 text-center py-3 text-gray-300">설명설명설명~</p>
+        </div>
+        <div className="flex flex-row w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center">
+            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Created At</h3>
+            <p className="flex-1 text-center py-3 text-gray-300">2024.03.14</p>
+        </div>
+        <div className="flex flex-col w-full mx-auto bg-violet-300 bg-opacity-30 rounded-lg itemes-center justify-center gap-2">
+            <h3 className="flex-1 text-center font-bold bg-violet-300 bg-opacity-40 rounded-l-lg py-3">Input Image</h3>
+            <img className="rounded-md" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        </div>
+
+    </div>
+
+    
     </div>
     </div>;
 }
