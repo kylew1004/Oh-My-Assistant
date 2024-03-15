191725bab1e7935f1c13e2e9906ed2daa133367d
Chloe Han <jh7316@nyu.edu>
2024-03-15 18:09:33 +0900

    feat : add model train feature

--- frontend/src/page/Train.js
+++ frontend/src/page/Train.js
@@ -1,10 +1,14 @@
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams, redirect} from 'react-router-dom';
 import {useState} from 'react';
 import {postModelTrain} from '../util/http.js';
 
 export default function Train(){
     const [files,setFiles] = useState([]);
+    const [isTraining, setIsTraining] = useState(false);
     const navigate = useNavigate();
+    const {webtoonName} = useParams();
+    console.log(webtoonName);
+
     // const urls = files.map((item)=>{
     //     return URL.createObjectURL(item);
     // })
@@ -29,39 +33,36 @@ export default function Train(){
 
     async function handleSubmit(){
         const data = new FormData();
-        data.append('webtoonName','웹툰명');
-        // data.append('images', files);
         files.forEach((file)=>{
             data.append('images', file);
         });
         
-        const result = await postModelTrain(data);
-
-        // console.log(result);
-        // return null
-
+        setIsTraining(true);
+        const result = await postModelTrain(webtoonName, data);
+        if(result==='tokenError') redirect('/auth');
+        setIsTraining(false);
 
         navigate('/assets');
 
     }
 
-    return <div className="flex flex-col w-auto h-full bg-gradient-to-tl from-[#5748B9] to-[#624F77] m-6 mx-8 rounded-3xl p-7 px-16 overflow-auto">
-        <h1 className=" text-white font-bold text-2xl mx-auto mb-6">Add your illustrations</h1>
+    return <div className="flex flex-col w-auto h-full bg-gradient-to-tl from-[#5748B9] to-[#624F77] m-6 mx-8 rounded-3xl p-7 px-16 overflow-hidden">
+        <h1 className=" text-white font-bold text- mx-auto mb-6">Add your illustrations</h1>
         <p className="text-gray-400 text-md mx-auto">해당 웹툰의 그림체, 채색 스타일이 잘 드러나는 웹툰 이미지를 업로드해주세요. 업로드 된 사진들은 모델 학습에 사용됩니다. <br /> (5장 이하로 업로드)</p>
         
         <div className="flex flex-col h-full">
-        <label className={`mx-auto my-4 rounded-full px-10 ${files.length>4 ? "text-gray-700 bg-gray-500" : "text-[#342C5A] bg-gradient-to-r from-[#F6C443] to-[#F3AC58] font-bold cursor-pointer"}`}>
-            <input className="hidden" type="file" id="input" name="input" multiple onChange={handleChange} disabled={files.length>4}/>
+        <label className={`mx-auto my-4 rounded-full px-10 ${files.length>9 ? "text-gray-700 bg-gray-500" : "text-[#342C5A] bg-gradient-to-r from-[#F6C443] to-[#F3AC58] font-bold cursor-pointer"}`}>
+            <input className="hidden" type="file" id="input" name="input" multiple onChange={handleChange} disabled={files.length>9}/>
             <p className="mx-1 my-2">Upload Image</p>
         </label>
 
         <h2 className=" text-white font-bold text-2xl mx-auto mt-3 mb-3">Preview</h2>
 
-        <ul className="flex flex-row px-5 flex-wrap py-3 h-[250px] md:grid-cols-3 no-scrollbar bg-white bg-opacity-50 overflow-scroll rounded-3xl">
+        <ul className="flex flex-row flex-wrap justify-start px-5 py-3 h-[50%] w-full mx-auto no-scrollbar bg-white bg-opacity-50 overflow-scroll rounded-3xl">
                 {
                     files.map((item,index)=>{
-                        return <li className="h-[170px] w-[170px] m-4 relative inline-block" key={index}> 
-                            <img className="h-[170px] w-[170px]" src={URL.createObjectURL(item)}/> 
+                        return <li className="flex w-[20%] aspect-w-1 aspect-h-1 m-4 relative " key={index}> 
+                            <img className=" h-full aspect-w-1 aspect-h-1  w-full" src={URL.createObjectURL(item)}/> 
                             <button className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 border-2 border-gray-500 text-gray-500 bg-white bg-opacity-50 rounded-full px-2 text-lg font-bold"
                             onClick={()=>handleImageClick(index)}>x</button> 
                             <input type="file" id="images" name="images" filename="hey" hidden/>
@@ -71,8 +72,8 @@ export default function Train(){
 
         </ul>
 
-        <button onClick={handleSubmit} disabled={files.length==0} className="mx-auto my-auto mb-auto rounded-full text-[#342C5A] text-xl h-auto py-3 px-12 mt-8
-    bg-gradient-to-r from-[#F19E39] to-[#E34F6B] font-bold cursor-pointer disabled:bg-gray-600">Start Model Training</button>
+        <button onClick={handleSubmit} disabled={files.length==0} className="mx-auto my-auto mb-auto rounded-full text-[#342C5A] text-xl h-[10%] py-3 px-12 mt-8
+    bg-gradient-to-r from-[#F19E39] to-[#E34F6B] font-bold cursor-pointer disabled:bg-gray-600">{isTraining ? 'Training...' : 'Start Model Training'}</button>
         </div>
         
 
