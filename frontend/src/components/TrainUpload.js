73e330137c54266c1e5ff9109c974e6ff7d9aef6
Chloe Han <jh7316@nyu.edu>
2024-03-17 19:33:38 +0900

    fix : fix styling

--- frontend/src/components/TrainUpload.js
+++ frontend/src/components/TrainUpload.js
@@ -11,7 +11,8 @@ export default function TrainUpload({handleState}){
 
     function handleChange(e){
         const imgFile = e.target.files[0];
-        const extension = imgFile.name.split('.').pop();
+        let extension;
+        if(imgFile.name) extension=imgFile.name.split('.').pop();
 
         if(ALLOW_EXTENSION.includes(extension)){
             setFiles((prev)=>{
@@ -55,22 +56,25 @@ export default function TrainUpload({handleState}){
         <h1 className=" text-white font-bold text-2xl mx-auto mb-6">Add your illustrations</h1>
         <p className="text-gray-400 text-md mx-auto">해당 웹툰의 그림체, 채색 스타일이 잘 드러나는 웹툰 이미지를 업로드해주세요. 업로드 된 사진들은 모델 학습에 사용됩니다. <br /> ** 허용 파일 확장자: jpg, jpeg, png  &nbsp;/&nbsp; 이미지 수: 1 - 10장 사이로 업로드</p>
         
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-full overflow-auto">
             <label className={`mx-auto my-4 rounded-full px-10 ${files.length>9 ? "text-gray-700 bg-gray-500" : "text-[#342C5A] bg-gradient-to-r from-[#F6C443] to-[#F3AC58] font-bold cursor-pointer"}`}>
                 <input className="hidden" type="file" id="input" name="input" multiple onChange={handleChange} disabled={files.length>9}/>
                 <p className="mx-1 my-2">Upload Image</p>
             </label>
 
-            <h2 className=" text-white font-bold text-2xl mx-auto mt-3 mb-3">Preview</h2>
+            <div className="flex flex-row mt-3 mb-3">
+                <h2 className="text-white font-bold text-2xl text-center ml-8 w-[90%]">Preview</h2>
+                <p className="my-auto ml-auto text-gray-300 mr-4">{files.length}/10</p>
+            </div>
+            
 
-            <ul className="flex flex-row flex-wrap justify-start px-5 py-3 min-h-[50%] max-h-[50%] w-full mx-auto no-scrollbar bg-white bg-opacity-50 overflow-scroll rounded-3xl">
+            <ul className="flex flex-row flex-wrap justify-start px-5 py-3 h-full w-full mx-auto no-scrollbar bg-white bg-opacity-50 overflow-scroll rounded-3xl">
                     {
                         files.map((item,index)=>{
-                            return <li className="flex w-[20%] aspect-square m-4 relative " key={index}> 
-                                <img className=" h-full aspect-w-1 aspect-h-1  w-full" src={URL.createObjectURL(item)}/> 
+                            return <li className="flex w-[20%] max-h-[60%] aspect-square m-4 relative " key={index}> 
+                                <img className="aspect-square" src={URL.createObjectURL(item)}/> 
                                 <button className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 border-2 border-gray-500 text-gray-500 bg-white bg-opacity-50 rounded-full px-2 text-lg font-bold"
                                 onClick={()=>handleImageClick(index)}>x</button> 
-                                <input type="file" id="images" name="images" filename="hey" hidden/>
                             </li>
                         })
                     }
