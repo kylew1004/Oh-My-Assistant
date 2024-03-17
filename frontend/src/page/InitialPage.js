b43dae06c48a89e4dccffb6258245d99d348a2b9
Chloe Han <jh7316@nyu.edu>
2024-03-18 05:20:20 +0900

    feat : update initialpage ui

--- frontend/src/page/InitialPage.js
+++ frontend/src/page/InitialPage.js
@@ -1,11 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState} from 'react';
 import { useNavigate, useRouteLoaderData, useActionData } from 'react-router-dom';
+import AddWebtoonModal from '../components/AddWebtoonModal';
 import noWebtoonImg from '../assets/no-webtoon.png';
 
 export default function InitialPage(){
     const navigate = useNavigate();
     const {webtoons} = useRouteLoaderData('root');
+    const [isModal,setIsModal] = useState(false);
     
+    function handleClose(){
+      setIsModal(false);
+    }
+
+    function handleOpen(){
+      setIsModal(true);
+    }
 
 
     const fetchData = () => {
@@ -26,12 +35,17 @@ export default function InitialPage(){
 
     },[]);
 
-    return (
-      <div className="flex flex-col h-full bg-white bg-opacity-30 rounded-2xl items-center justify-center gap-5 m-10">
-        <img src={noWebtoonImg} className=" h-1/4 w-3/5" />
+    return (<>
+      <AddWebtoonModal open={isModal} handleClose={handleClose}/>
+      <div className="flex flex-col h-full bg-white bg-opacity-30 rounded-2xl items-center justify-center m-10">
+        <img src={noWebtoonImg} className=" h-1/4 mb-14" />
         <h2 className=" text-[#19162a]">아직 생성된 웹툰이 없습니다.</h2>
         <p className=" text-[#19162a]">사이드 메뉴에서 새 웹툰 추가 후 서비스를 시작해 보세요!</p>
+        <button className="bg-violet-500 rounded-full px-8 py-4 mt-7 font-bold bg-gradient-to-r from-[#a7a1f9] to-[#c6aaf9]" onClick={handleOpen}>Add new webtoon</button>
       </div>
+    
+    </>
+      
     );
 
 }
