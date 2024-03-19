1b4dbb477ddb024783716c4a6bf6a628c27fa18c
Chloe Han <jh7316@nyu.edu>
2024-03-20 04:06:09 +0900

    feat : update styling

--- frontend/src/components/BackButton.js
+++ frontend/src/components/BackButton.js
@@ -6,9 +6,9 @@ import backIcon from '../assets/back.svg';
 export default function BackButton(){
     const navigate = useNavigate();
 
-    return <button onClick={() => navigate(-1)} className="flex flex-row gap-1 bg-yellow-500 rounded-full py-2 pl-4 pr-7 ml-10 hover:bg-yellow-600/70">
+    return <button onClick={() => navigate(-1)} className="flex flex-row gap-1 bg-yellow-500 rounded-full py-2 pl-4 pr-7 hover:bg-yellow-600/70">
                 <img src={backIcon} className="h-5 my-auto"/>
-                <p>Back</p>
+                <p className=" my-auto">Back</p>
             </button>
 
 }
