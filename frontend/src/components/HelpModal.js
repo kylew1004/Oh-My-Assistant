3d297ac908c7362eeae856552f3e65c1081c4e66
Chloe Han <jh7316@nyu.edu>
2024-03-20 18:04:34 +0900

    fix : fix styling

--- frontend/src/components/HelpModal.js
+++ frontend/src/components/HelpModal.js
@@ -12,7 +12,7 @@ export default function HelpModal({open, handleCloseModal}){
     
     return <div onClick={handleCloseModal}>
         <dialog class="rounded-lg max-w-[50%]" ref={mod} >
-            <div className="flex flex-col p-6 gap-4" onClick={e => e.stopPropagation()}>
+            <div className="flex flex-col p-6 gap-4 bg-gray-100" onClick={e => e.stopPropagation()}>
                 <div className="flex flex-row">
                     <div className="my-auto border-2 border-yellow-600 rounded-full px-1.5 text-yellow-600 text-sm mr-2">?</div>
                     <h2 className="text-md text-yellow-600 my-auto">Help</h2>
