4b6b2839817c05b19d96b8053e58208757d1d804
Chloe Han <jh7316@nyu.edu>
2024-03-27 18:24:21 +0900

    fix : fix error

--- frontend/src/page/Train.js
+++ frontend/src/page/Train.js
@@ -20,7 +20,7 @@ export default function Train() {
     return <div className="flex flex-col w-auto h-full bg-gradient-to-tl from-[#5748B9] to-[#624F77] m-6 mx-8 rounded-3xl p-7 px-16 overflow-hidden">
         {state===0 && <TrainUpload handleState={handleState} />}
         {isMutatingTrain ? <TrainLoading /> : null}
-        {state==2 && !isMutatingTrain && <TrainComplete />}
+        {state==2 && <TrainComplete />}
         {state===3 && !isMutatingTrain && <TrainError handleState={handleState}/>} 
     </div>
   );
