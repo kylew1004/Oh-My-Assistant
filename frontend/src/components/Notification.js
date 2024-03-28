baa8b7ad2507dcead8befb83097848d339949374
kylew1004 <kylew1004@gmail.com>
2024-03-28 14:12:54 +0900

    feat : implement MUI

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,7 +1,8 @@
-export default function Notification() {
-  return (
-    <div className="absolute inset-0 z-20 top-0 right-0 h-[10%] w-[10%]">
-      <p>Success!asfdsadfasdfds</p>
+import Alert from '@mui/material/Alert';
+
+export default function Notification(){
+    return <div className="absolute  z-20 top-0 right-0 m-4">
+        <Alert severity="success">Train completed successfully.</Alert>
     </div>
   );
 }
